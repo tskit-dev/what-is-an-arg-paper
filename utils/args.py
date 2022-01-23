5df20b6bad55a7dea6515e9f23e803337f2c07cc
Jerome Kelleher <jk@well.ox.ac.uk>
2022-01-23 10:23:00 +0000

    Factored out the node from Interval

--- utils/args.py
+++ utils/args.py
@@ -5,9 +5,8 @@ import random
 import math
 import dataclasses
 from typing import List
-import itertools
+from typing import Any
 
-import intervaltree
 import tskit
 
 NODE_IS_RECOMB = 1 << 1
@@ -15,28 +14,21 @@ NODE_IS_NONCOAL_CA = 1 << 2  # TODO better name
 
 
 @dataclasses.dataclass
-class Interval:
-    """
-    A class representing a interval.
-    """
-
+class AncestryInterval:
     left: int
     right: int
-    node: int = -1
-    ancestral_to: int = -1
+    ancestral_to: int
 
 
 @dataclasses.dataclass
 class Lineage:
     node: int
-    ancestry: List[Interval]
+    ancestry: List[AncestryInterval]
 
     def __str__(self):
         s = f"{self.node}:["
         for interval in self.ancestry:
-            s += str(
-                (interval.left, interval.right, interval.node, interval.ancestral_to)
-            )
+            s += str((interval.left, interval.right, interval.ancestral_to))
             s += ", "
         return s[:-2] + "]"
 
@@ -79,6 +71,13 @@ class Lineage:
         return Lineage(self.node, right_ancestry)
 
 
+@dataclasses.dataclass
+class MappingSegment:
+    left: int
+    right: int
+    value: Any = None
+
+
 def overlapping_segments(segments):
     """
     Returns an iterator over the (left, right, X) tuples describing the
@@ -87,7 +86,7 @@ def overlapping_segments(segments):
     S = sorted(segments, key=lambda x: x.left)
     n = len(S)
     # Insert a sentinel at the end for convenience.
-    S.append(Interval(math.inf, 0))
+    S.append(MappingSegment(math.inf, 0))
     right = S[0].left
     X = []
     j = 0
@@ -113,97 +112,96 @@ def overlapping_segments(segments):
             right = min(x.right for x in X)
             yield left, right, X
 
+
 def merge_ancestry(lineages):
-    node_map = {lineage.node: lineage for lineage in lineages}
     segments = []
     for lineage in lineages:
-        segments.extend(lineage.ancestry)
+        for interval in lineage.ancestry:
+            segments.append(
+                MappingSegment(interval.left, interval.right, (lineage, interval))
+            )
 
     for left, right, U in overlapping_segments(segments):
-        ancestral_to = sum(u.ancestral_to for u in U)
-        interval = Interval(left, right, -1, ancestral_to)
-        yield interval, [node_map[u.node] for u in U]
+        ancestral_to = sum(u.value[1].ancestral_to for u in U)
+        interval = AncestryInterval(left, right, ancestral_to)
+        yield interval, [u.value[0] for u in U]
+
+
+class ArgSimulator:
+    def __init__(self, num_samples, rho, L, seed):
+        self.num_samples = num_samples
+        self.L = L
+        self.rho = rho
+        self.rng = random.Random(seed)
+        self.tables = tskit.TableCollection(L)
+        self.tables.nodes.metadata_schema = tskit.MetadataSchema.permissive_json()
+        self.lineages = []
+        for _ in range(num_samples):
+            node = self.tables.nodes.add_row(time=0, flags=tskit.NODE_IS_SAMPLE)
+            self.lineages.append(Lineage(node, [AncestryInterval(0, L, 1)]))
+
+    def run(self):
+        t = 0
+        while len(self.lineages) > 0:
+            # print(f"t = {t:.2f} k = {len(lineages)}")
+            # for lineage in lineages:
+            #     print(f"\t{lineage}")
+            lineage_links = [lineage.num_recombination_links for lineage in self.lineages]
+            total_links = sum(lineage_links)
+            re_rate = total_links * self.rho
+            t_re = math.inf if re_rate == 0 else self.rng.expovariate(re_rate)
+            k = len(self.lineages)
+            ca_rate = k * (k - 1) / 2
+            t_ca = self.rng.expovariate(ca_rate)
+            t_inc = min(t_re, t_ca)
+            t += t_inc
+            if t_inc == t_re:
+                lineage = self.rng.choices(self.lineages, weights=lineage_links)[0]
+                breakpoint = self.rng.randrange(lineage.left + 1, lineage.right)
+                assert lineage.left < breakpoint < lineage.right
+                node = self.tables.nodes.add_row(
+                    flags=NODE_IS_RECOMB, time=t, metadata={"breakpoint": breakpoint}
+                )
+                right = lineage.split(breakpoint)
+                self.lineages.append(right)
+                for lineage in [lineage, right]:
+                    for interval in lineage.ancestry:
+                        self.tables.edges.add_row(
+                            interval.left, interval.right, node, lineage.node
+                        )
+                    lineage.node = node
+            else:
+                a = self.lineages.pop(self.rng.randrange(len(self.lineages)))
+                b = self.lineages.pop(self.rng.randrange(len(self.lineages)))
+                # print(f"\ta = {a}")
+                # print(f"\tb = {b}")
+                c = Lineage(len(self.tables.nodes), [])
+                flags = NODE_IS_NONCOAL_CA
+                for interval, intersecting_lineages in merge_ancestry([a, b]):
+                    if len(intersecting_lineages) > 1:
+                        flags = 0  # This is a coalescence, treat this as ordinary tree node
+                    if interval.ancestral_to < self.num_samples:
+                        c.ancestry.append(interval)
+                    for lineage in intersecting_lineages:
+                        self.tables.edges.add_row(
+                            interval.left, interval.right, c.node, lineage.node
+                        )
+                self.tables.nodes.add_row(flags=flags, time=t, metadata={})
+                # print(f"\tc = {c}")
+                if len(c.ancestry) > 0:
+                    self.lineages.append(c)
+
+        self.tables.sort()
+        return self.tables.tree_sequence()
 
 # NOTE! This hasn't been statistically tested and is probably not correct.
 def arg_sim(n, rho, L, seed=None):
-    rng = random.Random(seed)
-    tables = tskit.TableCollection(L)
-    tables.nodes.metadata_schema = tskit.MetadataSchema.permissive_json()
-    lineages = []
-    for _ in range(n):
-        node = tables.nodes.add_row(time=0, flags=tskit.NODE_IS_SAMPLE)
-        lineages.append(Lineage(node, [Interval(0, L, node, 1)]))
-
-    t = 0
-    while len(lineages) > 0:
-        print(f"t = {t:.2f} k = {len(lineages)}")
-        for lineage in lineages:
-            print(f"\t{lineage}")
-        lineage_links = [lineage.num_recombination_links for lineage in lineages]
-        total_links = sum(lineage_links)
-        re_rate = total_links * rho
-        t_re = math.inf if re_rate == 0 else rng.expovariate(re_rate)
-        k = len(lineages)
-        ca_rate = k * (k - 1) / 2
-        t_ca = rng.expovariate(ca_rate)
-        t_inc = min(t_re, t_ca)
-        t += t_inc
-        if t_inc == t_re:
-            # Choose a lineage to recombine with probability equal to the
-            # number of recombination links it subtends.
-            lineage = rng.choices(lineages, weights=lineage_links)[0]
-            # Choose a breakpoint uniformly on that lineage
-            breakpoint = rng.randrange(lineage.left + 1, lineage.right)
-            assert lineage.left < breakpoint < lineage.right
-            node = tables.nodes.add_row(
-                flags=NODE_IS_RECOMB, time=t, metadata={"breakpoint": breakpoint}
-            )
-            right = lineage.split(breakpoint)
-            lineages.append(right)
-            for lineage in [lineage, right]:
-                # lineage.node = node
-                for interval in lineage.ancestry:
-                    tables.edges.add_row(
-                        interval.left, interval.right, node, lineage.node
-                    )
-                    interval.node = node
-                lineage.node = node
-
-        else:
-            # print("CA EVENT")
-            a = lineages.pop(rng.randrange(len(lineages)))
-            b = lineages.pop(rng.randrange(len(lineages)))
-            print(f"\ta = {a}")
-            print(f"\tb = {b}")
-            ancestry = []
-            flags = NODE_IS_NONCOAL_CA
-            node = len(tables.nodes)
-            # for left, right, U in overlapping_segments(a.ancestry + b.ancestry):
-            for interval, intersecting_lineages in merge_ancestry([a, b]):
-                # ancestral_to = sum(u.ancestral_to for u in U)
-                # assert left < right
-                if len(intersecting_lineages) > 1:
-                    flags = 0  # This is a coalescence, treat this as ordinary tree node
-                if interval.ancestral_to < n:
-                    interval.node = node
-                    ancestry.append(interval)
-                for lineage in intersecting_lineages:
-                    tables.edges.add_row(interval.left, interval.right, node,
-                            lineage.node)
-            tables.nodes.add_row(flags=flags, time=t, metadata={})
-            # print("\tdone:", ancestry)
-            if len(ancestry) > 0:
-                c = Lineage(node, ancestry)
-                print(f"\tc = {c}")
-                lineages.append(c)
-    # print()
-    # print(tables)
-    tables.sort()
-    # tables.simplify()
-    return tables.tree_sequence()
+    sim = ArgSimulator(n, rho, L, seed)
+    return sim.run()
 
 
 ts = arg_sim(5, 0.2, 10, seed=234)
+print(ts.tables)
 
 node_labels = {}
 for node in ts.nodes():
