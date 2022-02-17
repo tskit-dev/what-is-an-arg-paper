eb9e22a1fc455a00db2e00904fcab367d0c0af0c
a-ignatieva <anastasia.ignatieva@stats.ox.ac.uk>
2022-02-17 15:38:14 +0000

    Tweaks to Figure 4

--- illustrations/ARG_edge_annotations.py
+++ illustrations/ARG_edge_annotations.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import tskit
+import PIL
 
 current_dir = Path(__file__).parent
 
@@ -57,9 +58,10 @@ def arg_edge_annotations():
                     font_size=16,
                     edge_labels={(e.child, e.parent): f"({e.left:.0f},{e.right:.0f}]" for e in ts.edges() if func1(e) and func2(e)},
                     horizontalalignment=halign,
+                    bbox=dict(boxstyle="round,pad=0.05", ec=(1.0, 1.0, 1.0), fc=(1.0, 1.0, 1.0)),
                 )        
     
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(25,13), sharey=True)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(26,18), sharey=True)
     
     ax1.set_title("(a) eARG with implicit\nencoding (Wiuf & Hein)")
     ts = argutils.viz.label_nodes(argutils.wh99_example())
@@ -67,13 +69,13 @@ def arg_edge_annotations():
         ts, ax1,
         use_ranked_times=False,
         node_color=mpl.colors.to_hex(plt.cm.tab20(1)),
-        node_size=450,
+        node_size=100,
         max_edge_width=2,
         font_size=14,
         tweak_x={
-            0: 10, 3: 29, 4: 2.5, 7: 20, 11: 10, 12: -18,
-            20: -23, 19: 4, 21: -11, 17: -25, 13: -16,
-            8: 2, 9: -24, 5: -16, 6: -41, 15: 1, 16: -26, 2: -10
+            0: 10, 3: 29, 4: 2.5, 7: 20, 11: 9, 12: -17,
+            20: -22.5, 19: 3.5, 21: -11, 17: -25, 13: -16,
+            8: 2, 9: -24, 5: -16, 6: -41, 15: 0.8, 16: -25.8, 2: -10
         }
     )
     add_edge_labels(ax1, ts, G, pos)
@@ -83,13 +85,106 @@ def arg_edge_annotations():
     pos, G = argutils.viz.draw(
         ts2, ax2, pos=pos,
         node_color=mpl.colors.to_hex(plt.cm.tab20(1)),
-        node_size=450,
+        node_size=200,
         font_size=14,
         #arrows=True,
         draw_edge_widths=True,
     )
     add_edge_labels(ax2, ts2, G, pos)
     
+    
+    # From https://networkx.org/documentation/stable/auto_examples/drawing/plot_custom_node_icons.html
+    icons = {
+        "genome_empty": "node_icons/genome_empty.png",
+        "genome_full": "node_icons/genome_full.png",
+        "genome_1": "node_icons/genome_1.png",
+        "genome_2": "node_icons/genome_2.png",
+        "genome_3": "node_icons/genome_3.png",
+        "genome_4": "node_icons/genome_4.png",
+        "genome_5": "node_icons/genome_5.png",
+        "genome_6": "node_icons/genome_6.png",
+        "genome_0-4": "node_icons/genome_0-4.png",
+        "genome_4-7": "node_icons/genome_4-7.png",
+        "genome_0-2": "node_icons/genome_0-2.png",
+        "genome_2-7": "node_icons/genome_2-7.png",
+        "genome_0-2-4-7": "node_icons/genome_0-2-4-7.png",
+        "genome_4-6": "node_icons/genome_4-6.png",
+        "genome_6-7": "node_icons/genome_6-7.png",
+        "genome_0-1": "node_icons/genome_0-1.png",
+        "genome_00-11": "node_icons/genome_00-11.png",
+        "genome_1-7": "node_icons/genome_1-7.png",
+        "genome_00-22-2-7": "node_icons/genome_00-22-2-7.png",
+        "genome_11-22-2-7": "node_icons/genome_11-22-2-7.png",
+        "genome_22-77": "node_icons/genome_22-77.png",
+        "genome_ancestral": "node_icons/genome_ancestral.png",
+    }
+    # Load images
+    images = {k: PIL.Image.open(fname) for k, fname in icons.items()}
+    # Panel (a)
+    tr_figure = ax1.transData.transform
+    tr_axes = fig.transFigure.inverted().transform
+    icon_size = (ax1.get_xlim()[1] - ax1.get_xlim()[0]) * 0.0002
+    icon_center = icon_size / 2.0
+    for n in G.nodes:
+        G.nodes[n]["image"] = images["genome_empty"]
+    for n in [19,20]:
+        G.nodes[n]["image"] = images["genome_1"]
+    for n in [5,6]:
+        G.nodes[n]["image"] = images["genome_2"]
+    for n in [11,12]:
+        G.nodes[n]["image"] = images["genome_3"]
+    for n in [3,4]:
+        G.nodes[n]["image"] = images["genome_4"]
+    for n in [8,9]:
+        G.nodes[n]["image"] = images["genome_5"]
+    for n in [15,16]:
+        G.nodes[n]["image"] = images["genome_6"]
+    for n in G.nodes:
+        xf, yf = tr_figure(pos[n])
+        xa, ya = tr_axes((xf, yf))
+        a = plt.axes([xa - icon_center, ya - icon_center, icon_size, icon_size])
+        a.imshow(G.nodes[n]["image"])
+        a.axis("off")
+        
+    # Panel (b)
+    tr_figure = ax2.transData.transform
+    tr_axes = fig.transFigure.inverted().transform
+    icon_size = (ax2.get_xlim()[1] - ax2.get_xlim()[0]) * 0.0002
+    icon_center = icon_size / 2.0
+    for n in [0, 1, 2, 7, 14, 18, 22]:
+        G.nodes[n]["image"] = images["genome_full"]
+    for n in [3]:
+        G.nodes[n]["image"] = images["genome_0-4"]
+    for n in [15]:
+        G.nodes[n]["image"] = images["genome_4-6"]
+    for n in [4, 12, 13]:
+        G.nodes[n]["image"] = images["genome_4-7"]
+    for n in [16]:
+        G.nodes[n]["image"] = images["genome_6-7"]
+    for n in [9]:
+        G.nodes[n]["image"] = images["genome_empty"]
+    for n in [10]:
+        G.nodes[n]["image"] = images["genome_0-2-4-7"]
+    for n in [5, 8, 11]:
+        G.nodes[n]["image"] = images["genome_0-2"]
+    for n in [19]:
+        G.nodes[n]["image"] = images["genome_00-11"]
+    for n in [20]:
+        G.nodes[n]["image"] = images["genome_11-22-2-7"]
+    for n in [21]:
+        G.nodes[n]["image"] = images["genome_22-77"]
+    for n in [6, 17]:
+        G.nodes[n]["image"] = images["genome_2-7"]
+    for n in [14, 18]:
+        G.nodes[n]["image"] = images["genome_00-22-2-7"]
+    for n in [22]:
+        G.nodes[n]["image"] = images["genome_ancestral"]
+    for n in G.nodes:
+        xf, yf = tr_figure(pos[n])
+        xa, ya = tr_axes((xf, yf))
+        a = plt.axes([xa - icon_center, ya - icon_center, icon_size, icon_size])
+        a.imshow(G.nodes[n]["image"])
+        a.axis("off")
 
     graph_io = io.StringIO()
     plt.savefig(graph_io, format="svg", bbox_inches='tight')
